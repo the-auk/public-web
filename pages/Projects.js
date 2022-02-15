7f9f730bbf3e39de680b52337ce4c6c0728b43b2
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2022-02-15 14:14:36 -0800

    Update Projects.js

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,63 +1,67 @@
 import styles from '../styles/Project.module.css'
 import Head from 'next/head'
-
+import Image from 'next/image'
+import React, {useState } from "react";
+import mixit from '../public/MIXIT.webp';
+import calen from '../public/calen.webp';
+import anti from '../public/anti.webp';
+import jsc from '../public/JSC.webp';
+import dom_pig from '../public/DOM.webp';
+import meals from '../public/meals.webp';
 export default function Projects() {
+  const[showSection, setshowSection] = useState("mixit")
+
     return (
       <div className={styles.wrapper}>
-        <div className={styles.proj}>
-        <div className={styles.projright}>
-          <img className={styles.mix} src = "MIXIT.png" />
-        </div>
-            <div className={styles.projleft}>
-              <p className={styles.header}> MIXIT</p>
-              <p>An Android Application that shows the user, recipes that can be made with the items in the user's kitchen.<br />
-                 Java | XML | SQLite | Picasso API | Android Studio
-              </p>
-        </div>
-        </div>
-        <div className={styles.proj}>
-            <div className={styles.projleft}>
-              <p className={styles.header}> Calendar App</p>
-              <p>A Desktop Application that acts as a calendar and lets the user schedule events as needed. <br />
-                 Swing Library with MVC and Strategy Pattern
-              </p>
-        </div>
-        <div className={styles.projright}>
-          <img className={styles.mix} src = "calen2.png" />
+        <div className={styles.projectleft}>
+          <div className={styles.headingWrapper}>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("mixit")}>MixIt</button></div>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("calend")}>Calendar_App</button></div>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("antispot")}>Anti_Spotify</button></div>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("budget")}>Monthly_Budget_App</button></div>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("piggame")}>Pig_Game</button></div>
+          <div className={styles.projectheading}>
+          <button onClick={()=> setshowSection("meals")}>theAUk_Meals</button></div>
           </div>
         </div>
-        <div className={styles.proj}>
-        <div className={styles.projright}>
-          <img className={styles.mix} src = "anti.png" />
-        </div>
-            <div className={styles.projleft}>
-              <p className={styles.header}> Anti Spotify</p>
-              <p>A web application that gives you song recommendations based on your playlist that Spotify never would.<br />
-                 NodeJS | HTML | Spotify's API | Frontend being updated in ReactJS | SCSS
-              </p>
-        </div>
-        </div>
-        <div className={styles.proj}>
-            <div className={styles.projleft}>
-              <p className={styles.header}> Monthly Budget App</p>
-              <p>A Web Application that lets the user put in expenses and income and gives outputs the budget.<br />
-                 HTML CSS Vanilla JavaScript
-              </p>
-        </div>
-        <div className={styles.projright}>
-          <img className={styles.mix} src = "JSC.PNG" />
+        <div className={styles.projectright}>
+          <div className={styles.imgwrapper}>
+          <div className={styles.projectimage}>
+          {showSection=="mixit" && <Image className={styles.projectimg}
+          src={mixit} alt="MixIt Android Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
           </div>
-        </div>
-        <div className={styles.proj}>
-        <div className={styles.projright}>
-          <img className={styles.mix} src = "DOM.PNG" />
-        </div>
-            <div className={styles.projleft}>
-              <p className={styles.header}> PIG Game</p>
-              <p>A simple browser game for two players to roll die and get higher score.<br />
-                 HTML | CSS | Vanilla JavaScript
-              </p>
-        </div>
+          <div className={styles.projectimage}>
+          {showSection=="calend" && <Image className={styles.projectimg}
+          src={calen} alt="Java Calendar Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="antispot" && <Image className={styles.projectimg}
+          src={anti} alt="Anti-Spotify Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="budget" && <Image className={styles.projectimg}
+          src={jsc} alt="Monthly Budget Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="piggame" && <Image className={styles.projectimg}
+          src={dom_pig} alt="Pig Game Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="meals" && <Image className={styles.projectimg}
+          src={meals} alt="Food Ordering App built on React" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          </div>
         </div>
         <Head>
           <title>Tanmay Ivar Siwach</title>
@@ -67,4 +71,3 @@ export default function Projects() {
       </div>
     )
   }
-  
