3ef361e11661bba402470f964b4877cf919d35a6
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2022-02-15 14:18:15 -0800

    Update Projects.js

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,11 +1,5 @@
 import styles from '../styles/Project.module.css'
 import Image from 'next/image'
-import mixit from '../public/MIXIT.webp';
-import calen from '../public/calen.webp';
-import anti from '../public/anti.webp';
-import jsc from '../public/JSC.webp';
-import dom_pig from '../public/DOM.webp';
-import meals from '../public/meals.webp';
 export default function Projects() {
   const[showSection, setshowSection] = useState("mixit")
 
@@ -27,40 +21,7 @@ export default function Projects() {
           <button onClick={()=> setshowSection("meals")}>theAUk_Meals</button></div>
           </div>
         </div>
-        <div className={styles.projectright}>
-          <div className={styles.imgwrapper}>
-          <div className={styles.projectimage}>
-          {showSection=="mixit" && <Image className={styles.projectimg}
-          src={mixit} alt="MixIt Android Application" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          <div className={styles.projectimage}>
-          {showSection=="calend" && <Image className={styles.projectimg}
-          src={calen} alt="Java Calendar Application" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          <div className={styles.projectimage}>
-          {showSection=="antispot" && <Image className={styles.projectimg}
-          src={anti} alt="Anti-Spotify Web Application" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          <div className={styles.projectimage}>
-          {showSection=="budget" && <Image className={styles.projectimg}
-          src={jsc} alt="Monthly Budget Web Application" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          <div className={styles.projectimage}>
-          {showSection=="piggame" && <Image className={styles.projectimg}
-          src={dom_pig} alt="Pig Game Web Application" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          <div className={styles.projectimage}>
-          {showSection=="meals" && <Image className={styles.projectimg}
-          src={meals} alt="Food Ordering App built on React" 
-          layout='fill' placeholder='blur' loading="eager" />}
-          </div>
-          </div>
-        </div>
+        
       </div>
     )
   }
