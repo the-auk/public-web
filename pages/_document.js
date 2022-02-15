6796efcb34b332e409d8682c9ec3dcfe0db7983d
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2022-02-15 14:29:44 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Allison&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
