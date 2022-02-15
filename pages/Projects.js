373a85c4bd3fab033a4952eef2088d0ca21c1c7c
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2022-02-15 14:24:20 -0800

    Update Projects.js

--- pages/Projects.js
+++ pages/Projects.js
@@ -24,6 +24,40 @@ export default function Projects() {
           <button onClick={()=> setshowSection("meals")}>theAUk_Meals</button></div>
           </div>
         </div>
+        <div className={styles.projectright}>
+          <div className={styles.imgwrapper}>
+          <div className={styles.projectimage}>
+          {showSection=="mixit" && <Image className={styles.projectimg}
+          src="/MIXIT.webp" alt="MixIt Android Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="calend" && <Image className={styles.projectimg}
+          src="/calen.webp" alt="Java Calendar Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="antispot" && <Image className={styles.projectimg}
+          src="/anti.webp" alt="Anti-Spotify Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="budget" && <Image className={styles.projectimg}
+          src="/JSC.webp" alt="Monthly Budget Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="piggame" && <Image className={styles.projectimg}
+          src="/DOM.webp" alt="Pig Game Web Application" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          <div className={styles.projectimage}>
+          {showSection=="meals" && <Image className={styles.projectimg}
+          src="meals.webp" alt="Food Ordering App built on React" 
+          layout='fill' placeholder='blur' loading="eager" />}
+          </div>
+          </div>
+        </div>
 
         <Head>
           <title>Tanmay Ivar Siwach</title>
